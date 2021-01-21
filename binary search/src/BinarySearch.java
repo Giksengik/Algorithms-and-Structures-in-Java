7f90a7e18c66107467732d71bc280c5efd305b2a
Egor <giksengik@gmail.com>
2021-01-21 19:34:23 +0300

    binary search

--- binary search/src/BinarySearch.java
+++ binary search/src/BinarySearch.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class BinarySearch {
+public class BinarySearch {
+    public static int search (int [] array, int key) {
+        int lo=0;
+        int hi= array.length-1;
+        while(lo <= hi){
+            int mid = lo + (hi - lo) / 2;
+            if (key < array[mid]) hi = mid - 1;
+            else if (key > array[mid]) lo = mid + 1;
+            else return mid;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
